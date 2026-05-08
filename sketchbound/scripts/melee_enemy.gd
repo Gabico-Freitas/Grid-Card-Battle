1b14469b00dea250912f1d9902ab972aa0ff2723
Otaviobz <o.zomer@edu.pucrs.br>
2026-05-08 18:53:19 -0300

    Add files via upload

---
+++ sketchbound/scripts/melee_enemy.gd
@@ -0,0 +1,22 @@
+extends Enemy
+
+var move_points : int
+
+func _ready() -> void:
+	hp = 20
+
+func do_turn() -> void:
+	move_points = 2
+	if _try_attack(): return
+	while(move_points > 0):
+		grid.move_right(self)
+		move_points -= 1
+		if _try_attack(): return
+
+func _try_attack() -> bool:
+	var target_cell = grid.get_entity_pos(self) + Vector2i(0, 1)
+	var entity = grid.get_entity(target_cell)
+	if entity is Player:
+		entity.take_damage(5)
+		return true
+	return false
