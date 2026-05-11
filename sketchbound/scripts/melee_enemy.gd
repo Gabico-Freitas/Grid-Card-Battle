c5227f3f42028e39d08cea909daf25611aa51a19
PedroGY01 <pgiovenardiyounes@gmail.com>
2026-05-11 18:48:57 -0300

    try_attack

--- sketchbound/scripts/melee_enemy.gd
+++ sketchbound/scripts/melee_enemy.gd
@@ -16,9 +16,16 @@ func do_turn() -> void:
 		if _try_attack(): return
 
 func _try_attack() -> bool:
-	var target_cell = grid.get_entity_pos(self) + Vector2i(0, 1)
-	var entity = grid.get_entity(target_cell)
-	if entity is Player:
-		entity.take_damage(5)
-		return true
+	var target_cell = grid.get_entity_pos(self) #+ Vector2i(0, 1)
+	for i in range(-1, 2):
+		for j in range(-1, 2):
+			if i== 0 and j ==0:
+				continue
+				if i !=0 and j != 0:
+					continue
+			target_cell = target_cell + Vector2i(i, j)
+			var entity = grid.get_entity(target_cell)
+			if entity is Player:
+				entity.take_damage(5)
+				return true
 	return false
