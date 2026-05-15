c555de4a53b41b155180bcee32b0d0bd24b10bc1
PedroGY01 <pgiovenardiyounes@gmail.com>
2026-05-15 19:01:44 -0300

    Add files via upload

---
+++ sketchbound/scripts/range_enemy.gd
@@ -0,0 +1,22 @@
+extends Enemy
+
+var move_points : int
+
+func _ready() -> void:
+	hp = 15
+
+func do_turn() -> void:
+	move_points = 1
+	if _try_attack(): return
+	while(move_points > 0):
+		grid.move_right(self)
+		move_points -= 1
+		if _try_attack(): return
+
+func _try_attack() -> bool:
+	var target_cell = grid.get_entity_pos(self) + Vector2i(0, 1)
+	var entity = grid.get_entity(target_cell)
+	if entity is Player:
+		entity.take_damage(8)
+		return true
+	return false
