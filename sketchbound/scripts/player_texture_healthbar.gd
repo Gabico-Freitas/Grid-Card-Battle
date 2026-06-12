4b8494bdc68508a478da05f7e6675fd07b6dd539
Gabriel Freitas Da Silva <24102256@linux-ep.portoalegre.pucrsnet.br>
2026-06-12 18:58:53 -0300

    tela de controle e shake da vida

--- sketchbound/scripts/player_texture_healthbar.gd
+++ sketchbound/scripts/player_texture_healthbar.gd
@@ -3,6 +3,11 @@ const MAX_HP = 36
 var currHealth = MAX_HP
 @onready var health = $PlayerHealth
 
+@export var shake_amount: float = 8.0
+@export var shake_duration: float = 0.2
+@onready var default_position: Vector2 = position
+var tween: Tween
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.max_value = MAX_HP
@@ -23,3 +28,19 @@ func _on_player_damaged(value: int) -> void:
 	currHealth = value
 	set_health_bar()
 	set_health_label()
+	#shake()
+
+func reset_tween() -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween()
+
+func shake():
+	reset_tween()
+	var elapsed_time = 0.0
+	while elapsed_time < shake_duration:
+		var random_offset = Vector2(randf_range(-shake_amount, shake_amount), randf_range(-shake_amount, shake_amount))
+		position = default_position + random_offset
+		await get_tree().process_frame
+		elapsed_time += get_process_delta_time()
+	position = default_position
